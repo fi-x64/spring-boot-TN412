42029483f26186b3147cc799b58f82aa7c7f1d1e
fi.x64 <hohoangphia@gmail.com>
2022-12-01 23:20:21 +0700

    Edit BookableController

--- src/main/java/thud/controller/BookableController.java
+++ src/main/java/thud/controller/BookableController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +49,22 @@ public class BookableController {
 
         if (bookableData.isPresent()) {
             return new ResponseEntity<>(bookableData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/boolables/{id}")
+    public ResponseEntity<Bookable> updateTutorial(@PathVariable("id") long id, @RequestBody Bookable bookable) {
+        Optional<Bookable> bookableData = bookableRepository.findById(id);
+
+        if (bookableData.isPresent()) {
+            Bookable _bookable = bookableData.get();
+            _bookable.setGroup(bookable.getGroup());
+            _bookable.setImg(bookable.getImg());
+            _bookable.setTitle(bookable.getTitle());
+            _bookable.setNotes(bookable.getNotes());
+            return new ResponseEntity<>(bookableRepository.save(_bookable), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
